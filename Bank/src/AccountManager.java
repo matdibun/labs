9c550bbde76e5aa3f3b39dbf01968625593810a1
matdibun <adibgempakzz@gmail.com>
2024-10-15 00:09:20 +0900

    bank

--- Bank/src/AccountManager.java
+++ Bank/src/AccountManager.java
@@ -1,25 +1,9 @@
 public class AccountManager {
     public static void main(String[] args) {
-        BankAccount tester = new BankAccount(0);
-        System.out.println("잔액 = " + tester.getBalance());
-
-        int five = 50000;
-        int three = 30000;
-
-        if (tester.deposit(five))
-            System.out.println(five + " 원 입금 성공 : 잔액 = " + tester.getBalance());
-        else
-            System.out.println(five + " 원 입금 실패 : 잔액 = " + tester.getBalance());
-
-        if (tester.withdraw(three))
-            System.out.println(three + " 원 출금 성공 : 잔액 = " + tester.getBalance());
-        else
-            System.out.println(three + " 원 출금 실패 : 잔액 = " + tester.getBalance());
-
-        if (tester.withdraw(three))
-            System.out.println(three + " 원 출금 성공 : 잔액 = " + tester.getBalance());
-        else
-            System.out.println(three + " 원 출금 실패 : 잔액 = " + tester.getBalance());
+    String a = "Hello";
+    String b = a.substring(0,5);
+    int c = a.length();
+    System.out.println(b + c);
     }
 
 }
--- Bank/src/BankReader.java
+++ Bank/src/BankReader.java
@@ -1,2 +1,23 @@
+import javax.swing.*;
+
 public class BankReader {
+    String inputLine;
+    public BankReader() {
+        inputLine = "";
+    }
+    char readCommand(String message){
+        inputLine = JOptionPane.showInputDialog(message).trim().toUpperCase();
+        return inputLine.charAt(0);
+    }
+
+    int readAmount(){
+        int amount = 0;
+        String s = inputLine.substring(1, inputLine.length());
+        s = s.trim();
+        if (s.length() > 0){
+            amount = Integer.parseInt(s);
+        }else
+            JOptionPane.showMessageDialog(null, "Invalid amount entered.");
+        return amount;
+    }
 }
